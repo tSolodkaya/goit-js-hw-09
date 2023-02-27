4a4581f537d840263a3068c9f040b92d7788a750
tSolodkaya <77572918+tSolodkaya@users.noreply.github.com>
2023-02-27 14:47:03 +0000

    Deploying to gh-pages from @ tSolodkaya/goit-js-hw-09@92ea0def0f4850219e3ee9e7966f5a533aec92b4 🚀

---
+++ 01-color-switcher.56be972a.js
@@ -0,0 +1,2 @@
+!function(){const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(t){n=setInterval(e,1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1}));let n=null;function e(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.startBtn.disabled=!0}}();
+//# sourceMappingURL=01-color-switcher.56be972a.js.map
